ecb5824d6a1252c8b6585fdff364045406e04310
julien <julien-34@hotmail.fr>
2015-12-05 19:56:29 +0100

    Ajout d'une grid pour les CommandesFournisseur

---
+++ src/achat/CommandesFournisseur.java
@@ -0,0 +1,57 @@
+package achat;
+
+import java.sql.Date;
+
+public class CommandesFournisseur {
+	
+	private String refCommande, refFournisseur, nomFourniseur, montantTotal;
+	private Date date;
+
+	public CommandesFournisseur(String refCommande, Date date, String refFournisseur, String nomFournisseur, String montantTotal){
+		this.refCommande = refCommande;
+		this.date = date;
+		this.refFournisseur = refFournisseur;
+		this.nomFourniseur = nomFournisseur;
+		this.montantTotal = montantTotal;
+	}
+	
+	public String getMontantTotal() {
+		return montantTotal;
+	}
+
+	public void setMontantTotal(String montantTotal) {
+		this.montantTotal = montantTotal;
+	}
+	
+	public String getRefCommande() {
+		return refCommande;
+	}
+
+	public void setRefCommande(String refCommande) {
+		this.refCommande = refCommande;
+	}
+
+	public String getRefFournisseur() {
+		return refFournisseur;
+	}
+
+	public void setRefFournisseur(String refFournisseur) {
+		this.refFournisseur = refFournisseur;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getNomFourniseur() {
+		return nomFourniseur;
+	}
+
+	public void setNomFourniseur(String nomFourniseur) {
+		this.nomFourniseur = nomFourniseur;
+	}
+}
--- src/achat/PanelCommande.java
+++ src/achat/PanelCommande.java
@@ -1,16 +1,33 @@
 package achat;
 
+import java.awt.Dimension;
+import java.awt.ScrollPane;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.sql.Date;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
 
 import jdbc.DatabaseConnection;
 import principal.FenetrePrincipale;
 
 public class PanelCommande extends JPanel{
+	
+	private ArrayList<CommandesFournisseur> listeCommandes = new ArrayList<CommandesFournisseur>();
+	private Object[][] tabCo;
+	private Object[] titres = {"N° Commande","Fournisseur","Date", "Montant"};
+	
+	//On créer la JTable et son modèle
+	private static JTable tableau = new JTable(new DefaultTableModel());
+	private static UneditableTableModel modele;
+	private JScrollPane scrollPane;
 
 	public PanelCommande(FenetrePrincipale f){
 		
@@ -18,7 +35,7 @@ public class PanelCommande extends JPanel{
 		this.getCommande();
 				
 		//On remplit la JTable
-		//this.remplirTableau();
+		this.remplirTableau();
 				
 		//On initialise l'ensemble des composants sur le JPanel
 		//this.initElements();
@@ -28,19 +45,60 @@ public class PanelCommande extends JPanel{
 	}
 	
 	
+	/**
+	 * Méthode qui récupère l'ensemble des commandes de la base de données et les ajoute dans une ArrayList.
+	 */
 	private void getCommande(){
 		
 		try {
 			Connection cn = DatabaseConnection.getCon();
-			Statement st = cn.createStatement();
-			ResultSet rs = st.executeQuery("");
+			PreparedStatement pst = cn.prepareStatement("SELECT c.refCommande, c.dateCommande, f.refFournisseur, f.nomFournisseur, SUM(p.prixProduit*lc.quantite) AS montantTotal FROM CommandesFournisseur c JOIN Fournisseurs f ON f.refFournisseur = c.refFournisseur JOIN LignesCommandeFournisseur lc ON lc.refCommande = c.refCommande JOIN ProduitsFournisseur p ON p.refProduit = lc.refProduit GROUP BY c.refCommande, c.dateCommande, f.refFournisseur, f.nomFournisseur ORDER BY c.dateCommande DESC");
+			ResultSet rs = pst.executeQuery();
 			
 			
-			
+			while(rs.next()){
+
+				String refCommande = rs.getString("refCommande");
+				Date date = rs.getDate("dateCommande");
+				String refFournisseur = rs.getString("refFournisseur");
+				String nomFournisseur = rs.getString("nomFournisseur");
+				String montantTotal = rs.getString("montantTotal")+"€";
+
+				this.listeCommandes.add(new CommandesFournisseur(refCommande, date, refFournisseur, nomFournisseur, montantTotal));
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
 	}
 	
+	
+	/**
+	 * Méthode qui remplit le tableau avec les valeurs de l'arraylist, mais aussi la JTable.
+	 */
+	private void remplirTableau(){
+		
+		int nbDeCo = this.listeCommandes.size();//On calcule la taille de l'arrylist pour créer un tableau adéquat
+		this.tabCo = new Object[nbDeCo][4];//On créer le tableau de la taille récupérée 
+		
+		//On remplit ce dernier avec les CommandesFournisseur récupérées
+		for(CommandesFournisseur cf : this.listeCommandes){
+			this.tabCo[this.listeCommandes.indexOf(cf)][0] = cf.getRefCommande();
+			this.tabCo[this.listeCommandes.indexOf(cf)][1] = cf.getNomFourniseur();
+			this.tabCo[this.listeCommandes.indexOf(cf)][2] = cf.getDate();
+			this.tabCo[this.listeCommandes.indexOf(cf)][3] = cf.getMontantTotal();
+		}
+		
+		
+		modele = new UneditableTableModel(0,5);
+		modele.setDataVector(tabCo,titres);
+		
+		tableau = new JTable(modele);
+		
+		tableau.setAutoCreateRowSorter(false);
+		tableau.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		tableau.setPreferredScrollableViewportSize(new Dimension(800, 224));
+		
+		scrollPane = new JScrollPane(tableau);
+		this.add(scrollPane);
+	}
 }
